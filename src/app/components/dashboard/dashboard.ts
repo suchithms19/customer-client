43292ec44a5ff7c159c39e6b418df69132d0737b
Sanjana B <sab@kaplaninc.com>
2025-09-09 07:58:10 +0530

    created form in dashboard create tab

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule
+    
+  ],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.scss'
+  styleUrls: ['./dashboard.scss']
 })
 export class Dashboard {
+  dashboardForm: FormGroup;
+  
+  constructor(private fb: FormBuilder) {
+    this.dashboardForm = this.fb.group({
+      name: [''],
+      gender: [''],
+      dob: [''],
+      mobile: ['']
+    });
+  }
 
+  onSubmit() {
+    if (this.dashboardForm.valid) {
+      console.log(this.dashboardForm.value);
+      this.dashboardForm.reset();
+    }
+  }
 }
